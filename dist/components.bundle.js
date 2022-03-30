ca1cec7750b00cbe388e62ee4173efcaa727c20e
jenadiusnicholaus <ucode888@gmail.com>
2022-03-30 20:07:51 +0300

    fix lighthouse part 2

--- dist/components.bundle.js
+++ dist/components.bundle.js
@@ -16,14 +16,45 @@
   \***************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n\n\nconst renderTodo = (todolist) => {\n  let todolistEl = document.querySelector('.todolist-ul');\n  todolistEl.innerHTML= '';\n  let liElement = '';\n  for( let i = 0; i< todolist.length; i += 1) {\n    const liEl = document.createElement('LI');\n    liEl.classList.add('todoitem')\n\n    liElement +=`<li>\n      <label class=\"container\">${todolist[i].description}\n        <input type=\"checkbox\">\n        <span class=\"checkmark\"></span>\n      </label>\n    </li>`\n  }\n  todolistEl.innerHTML = liElement;\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (renderTodo());\n\n//# sourceURL=webpack://work-with-webpack/./src/components.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _data_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./data.js */ \"./src/data.js\");\n\n\nconst renderTodo = () => {\n  const todolistEl = document.querySelector('.todolist-ul');\n  todolistEl.innerHTML = '';\n  let liElement = '';\n  for (let i = 0; i < _data_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"].length; i += 1) {\n    const liEl = document.createElement('LI');\n    liEl.classList.add('todoitem');\n\n    liElement += `<li class=\"todoitem\">\n    <div class=\"card-body\">\n      <div class=\"content\">\n        <input type=\"checkbox\">\n        <label class=\"lable\">${_data_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"][i].description}</label>\n      </div>\n      <svg  xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-three-dots-vertical menu-icon\" viewBox=\"0 0 16 16\">\n        <path d=\"M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z\"/>\n      </svg>\n    </div> \n    </li>`;\n  }\n  todolistEl.innerHTML = liElement;\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (renderTodo);\n\n//# sourceURL=webpack://work-with-webpack/./src/components.js?");
+
+/***/ }),
+
+/***/ "./src/data.js":
+/*!*********************!*\
+  !*** ./src/data.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst todolist = [\n  {\n    description: 'Learning webpack',\n    completed: false,\n    index: 0,\n  },\n\n  {\n    description: 'Learning softskills',\n    completed: false,\n    index: 0,\n  },\n  {\n    description: 'Learning webpack',\n    completed: false,\n    index: 0,\n  },\n\n  {\n    description: 'Learning Datastructure',\n    completed: false,\n    index: 0,\n  },\n];\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (todolist);\n\n//# sourceURL=webpack://work-with-webpack/./src/data.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -59,8 +90,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/components.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/components.js");
 /******/ 	
 /******/ })()
 ;
