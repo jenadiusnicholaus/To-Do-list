22f7f3c9c3264edf17805b35585a76eac3e3cc06
jenadiusnicholaus <ucode888@gmail.com>
2022-04-05 11:15:04 +0300

    prepare for deployment

--- src/components/localstorage.js
+++
@@ -1,7 +0,0 @@
-export default class TodoStore {
-    static getFromStorage= () => JSON.parse(localStorage.getItem('todos') || '[]');
-
-    static addToStorage = (todolistarray) => {
-      localStorage.setItem('todos', JSON.stringify(todolistarray));
-    }
-}
